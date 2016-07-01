d3e1afb23334505dd92d3e2f28baaff2fbb233e2
zevwings <zev.wings@gmail.com>
2016-07-01 21:59:41 +0800

    Initial Commit

---
+++ ZVDatabase/ZVDatabase.h
@@ -0,0 +1,19 @@
+//
+//  ZVDatabase.h
+//  ZVDatabase
+//
+//  Created by ZERO on 16/7/1.
+//  Copyright © 2016年 小零心语. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZVDatabase.
+FOUNDATION_EXPORT double ZVDatabaseVersionNumber;
+
+//! Project version string for ZVDatabase.
+FOUNDATION_EXPORT const unsigned char ZVDatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZVDatabase/PublicHeader.h>
+
+
---
+++ ZVDatabaseTests/ZVDatabaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZVDatabaseTests.swift
+//  ZVDatabaseTests
+//
+//  Created by ZERO on 16/7/1.
+//  Copyright © 2016年 小零心语. All rights reserved.
+//
+
+import XCTest
+@testable import ZVDatabase
+
+class ZVDatabaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
