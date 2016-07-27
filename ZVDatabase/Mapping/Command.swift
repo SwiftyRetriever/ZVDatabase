97a91a06c062c500302060784e7bd65be746bb57
zevwings <zev.wings@gmail.com>
2016-07-28 00:05:08 +0800

    add command.

--- ZVDatabase/Mapping/Command.swift
+++ ZVDatabase/Mapping/Command.swift
@@ -13,11 +13,30 @@ public class Command: NSObject {
     private var _sql: String = ""
     private var _parameters = [Bindable]()
     
-    public func select(_ column: [String], from table: String, parameters: [Bindable] = []) -> Command {
+    public func insert(_ column: [String], into table: String, parameters: [Bindable] = []) -> Command {
         
-        let sql = "SELECT \(column.joined(separator: ", ")) FROM \(table)"
+        let prefix = column.map { _ in return "?" }
+        let sql = "INSERT INTO \(table) (\(column.joined(separator: ", "))) VALUES (\(prefix.joined(separator: ", ")))"
         _sql.append(sql)
         _parameters.append(contentsOf: parameters)
+        
+        return self
+    }
+    
+    public func insert(_ values: [String: Bindable], into table: String) -> Command {
+        
+        var columnArray = [String]()
+        var prefixArray = [String]()
+        var valueArray  = [Bindable]()
+        
+        for (column, value) in values {
+            columnArray.append(column)
+            prefixArray.append(",")
+            valueArray.append(value)
+        }
+        
+        _sql.append("INSERT INTO \(table) \(columnArray.joined(separator: ",")) VALUES (\(prefixArray.joined(separator: ", ")))")
+        _parameters.append(contentsOf: valueArray)
         
         return self
     }
@@ -25,17 +44,24 @@ public class Command: NSObject {
     public func update(_ column: [String], table: String, parameters: [Bindable] = []) -> Command {
         
         let col = column.map { (col) in return "\(col) = ?" }.joined(separator: ", ")
-        let sql = "UPDATE \(table) SET \(col)"
-        _sql.append(sql)
+        _sql.append("UPDATE \(table) SET \(col)")
+        _parameters.append(contentsOf: parameters)
         
         return self
     }
     
-    public func insert(_ column: [String], into table: String, parameters: [Bindable] = []) -> Command {
+    public func update(_ values: [String: Bindable], table: String) -> Command {
         
-        let prefix = column.map { _ in return "?" }
-        let sql = "INSERT INTO \(table) (\(column.joined(separator: ", "))) VALUES (\(prefix.joined(separator: ", ")))"
-        _sql.append(sql)
+        var columnArray = [String]()
+        var valueArray  = [Bindable]()
+        
+        for (column, value) in values {
+            columnArray.append("\(column) = ?")
+            valueArray.append(value)
+        }
+        
+        _sql.append("UPDATE \(table) SET \(columnArray.joined(separator: ","))")
+        _parameters.append(contentsOf: valueArray)
         
         return self
     }
@@ -48,11 +74,28 @@ public class Command: NSObject {
         return self
     }
     
+    public func select(_ column: [String], from table: String, parameters: [Bindable] = []) -> Command {
+        
+        let sql = "SELECT \(column.joined(separator: ", ")) FROM \(table)"
+        _sql.append(sql)
+        _parameters.append(contentsOf: parameters)
+        
+        return self
+    }
+    
     public func `where`(_ expression: String, parameters: [Bindable] = []) -> Command {
         
         _sql.append(" WHERE ")
         _sql.append(expression)
         _parameters.append(contentsOf: parameters)
+        
+        return self
+    }
+    
+    public func order(by condition: [String]) -> Command {
+        
+        _sql.append(" ORDER BY ")
+        _sql.append(condition.joined(separator: ","))
         
         return self
     }
